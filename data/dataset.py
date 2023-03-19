6213bd70af4592313070f04d9798fe2c080962c2
gnom2134 <gnom21345@gmail.com>
2023-03-19 18:24:08 +0300

    Add Places365 customized dataset

---
+++ data/dataset.py
@@ -0,0 +1,16 @@
+from torchvision.datasets.places365 import Places365
+from pathlib import Path
+
+import numpy as np
+import torch
+
+
+class Places365Embedding(Places365):
+    def __init__(self, embeddings_path: Path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.embeddings = torch.from_numpy(np.load(str(embeddings_path)))
+
+    def __getitem__(self, item):
+        img, label = super(Places365Embedding, self).__getitem__(item)
+        embedding = self.embeddings[item, :]
+        return img, label, embedding
